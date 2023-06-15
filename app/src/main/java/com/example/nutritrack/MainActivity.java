7169151f2613ec2c5c6beb423c3f2a54a6b13008
bengisu <bengisugelin@gmail.com>
2023-06-15 13:37:49 -0700

    progress bar added. Main activity created

---
+++ app/src/main/java/com/example/nutritrack/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.nutritrack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    // colors: #a4c7a5 - #94c7c0 - #bcded0 - #e0f2f5 - #cbe9ed
+    //
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
